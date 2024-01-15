1e809756aa1c6061fc587585367feaea7dc0965f
Manavver <39192013+Manavver@users.noreply.github.com>
2024-01-16 01:59:57 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,12 @@
+#import mymodule
+
+from mymodule import greeting,greeting1,date,hi
+#mymodule.greeting("Vivek")
+
+#mymodule.greeting1("delhi")
+
+greeting("Vivek1")
+greeting1("delhi17")
+date("12-01-2024")
+
+hi()
---
+++ mymodule.py
@@ -0,0 +1,13 @@
+def greeting(name):
+  print("Hello, " + name)
+
+
+def greeting1(address):
+  print("Hello, my address =  " + address)
+
+
+def date(data):
+  print("Hello, today the date is   " + data)
+
+def hi():
+  print("Hello,hi how are you ")
